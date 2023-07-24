f235faa99c668bf8e60e82b6a54dc31c57d77c9c
Mervenur <ylmz.merwusch@gmail.com>
2023-07-24 15:34:54 +0300

    Initial commit

---
+++ MVVM/Viewmodel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ModernWPFDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWPFDesign.MVVM.Viewmodel
+{
+    internal class MainViewModel:ObservableObject
+    {
+
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public RelayCommand FeaturesViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+        public FeaturesViewModel FeaturesVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            FeaturesVM = new FeaturesViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+            FeaturesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = FeaturesVM;
+            });
+        }
+    }
+}
